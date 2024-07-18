859d79d8c7334f120a5dedd397624cbca8c7bcb5
Jonathan Cook <jcookbme@gmail.com>
2024-07-18 11:14:38 -0400

    Add tox and pytest test env

--- tests/test_home.py
+++ tests/test_home.py
@@ -3,13 +3,11 @@
 # pylint: disable=unused-argument
 from multiprocessing import Process
 
-import pytest
 import requests
 
 from tests.library import test_globals
 
 
-@pytest.mark.parametrize("base_access_app", ["users_schema"], indirect=True)
 def test_home(base_access_app: Process) -> None:
     """test home endpoint"""
     resp = requests.get(f"{test_globals.DEFAULT_URL}/home", timeout=5)
