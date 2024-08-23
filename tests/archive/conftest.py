629c432831fe2e67589c3c534c4378b3bb221e10
Jonathan Cook <jcookbme@gmail.com>
2024-08-23 07:19:46 -0400

    move everything to archive

--- tests/archive/conftest.py
+++ tests/archive/conftest.py
@@ -7,7 +7,7 @@ from typing import Dict, Iterator, List, Union
 
 import pytest
 
-from restful_budget_api.__app__ import create_api, create_app
+from restful_budget_api.archive.__app__ import create_api, create_app
 from tests.library.db_setup import make_db
 
 
